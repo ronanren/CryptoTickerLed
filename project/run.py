003199913b0cff2409e20650153f53ec047309ec
ronanren <renouxpro@gmail.com>
2023-09-08 17:05:48 +0100

    add custom font

--- project/run.py
+++ project/run.py
@@ -8,28 +8,24 @@ import time
 class Run(SampleBase):
     def __init__(self, *args, **kwargs):
         super(Run, self).__init__(*args, **kwargs)
-        self.parser.add_argument("-t", "--text", help="The text to scroll on the RGB LED panel", default="Hello world!")
 
     def run(self):
         offscreen_canvas = self.matrix.CreateFrameCanvas()
         font = graphics.Font()
-        font.LoadFont("../matrix/fonts/7x13.bdf")
-        textColor = graphics.Color(255, 255, 0)
-        pos = offscreen_canvas.width
-        my_text = self.args.text
+        font.LoadFont("fonts/6x12.bdf")
+        textColor = graphics.Color(255, 255, 255)
+        pos = 0
+        header = "AAPL -0.24%"
+        price = "$143.52"
 
         while True:
             offscreen_canvas.Clear()
-            len = graphics.DrawText(offscreen_canvas, font, pos, 10, textColor, my_text)
-            pos -= 1
-            if (pos + len < 0):
-                pos = offscreen_canvas.width
+            size_header = graphics.DrawText(offscreen_canvas, font, 1, 8, textColor, header)
+            size_price = graphics.DrawText(offscreen_canvas, font, 1, 16, textColor, price)
 
-            time.sleep(0.05)
+            time.sleep(0.1)
             offscreen_canvas = self.matrix.SwapOnVSync(offscreen_canvas)
 
-
-# Main function
 if __name__ == "__main__":
     run = Run()
     if (not run.process()):
