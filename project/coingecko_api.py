a6d48b808eccdfe982049c13d4b023a90fbf2057
ronanren <renouxpro@gmail.com>
2023-09-12 18:03:48 +0200

    update api

--- project/coingecko_api.py
+++ project/coingecko_api.py
@@ -7,34 +7,38 @@ def get_coins_lists():
     return response.json()
   
 
-def get_crypto_data(ticker):
+def get_crypto_data(ticker, interval="1h"):
     # historical data
-    url = "https://api.coingecko.com/api/v3/coins/%s/market_chart?vs_currency=usd&days=3" % ticker
+    url = "https://api.coingecko.com/api/v3/coins/%s/market_chart?vs_currency=usd&days=" % ticker
+    if interval == "1h":
+      url += "3"
+    elif interval == "5min":
+       url += "1"
     response = requests.get(url)
     data = response.json()
-    prices_by_hour = []
+    prices_by_interval = []
     for i in range(0, len(data["prices"])):
       data_hour = data["prices"][len(data["prices"]) - i - 1]
       data_hour[0] = datetime.fromtimestamp(data_hour[0] / 1000).strftime("%Y-%m-%d %H:%M:%S")
-      prices_by_hour.append(data_hour)
+      prices_by_interval.append(data_hour)
 
     # current price
     url = "https://api.coingecko.com/api/v3/coins/%s" % ticker
     response = requests.get(url)
     data = response.json()["market_data"]["current_price"]["usd"]
-    prices_by_hour[0] = [datetime.now().strftime("%Y-%m-%d %H:%M:%S"), data]
-    prices_by_hour = prices_by_hour[:64][::-1]
+    prices_by_interval[0] = [datetime.now().strftime("%Y-%m-%d %H:%M:%S"), data]
+    prices_by_interval = prices_by_interval[:64][::-1]
     price_change_percentage_24h = response.json()["market_data"]["price_change_percentage_24h"]
     
     # graph candle data
-    prices = [entry[1] for entry in prices_by_hour]
+    prices = [entry[1] for entry in prices_by_interval]
     min_price = min(prices)
     max_price = max(prices)
     max_height = 15
     scale_factor = (max_price - min_price) / max_height
     
     chart = []
-    for data_hour in prices_by_hour:
+    for data_hour in prices_by_interval:
         body_height = int((data_hour[1] - min_price) / scale_factor) + 1
         chart.append(body_height)
 
@@ -42,7 +46,6 @@ def get_crypto_data(ticker):
       "ticker": ticker,
       "current_price": data,
       "price_change_percentage_24h": price_change_percentage_24h,
-      "prices_by_hour": prices_by_hour,
       "chart": chart
     }
 
@@ -57,6 +60,6 @@ def plot_candlestick(chart, max_height=15):
     for row in chart_console:
         print(''.join(row))
    
-data = get_crypto_data("avalanche-2")
+data = get_crypto_data("ethereum", "5min")
 plot_candlestick(data["chart"])
 # print(data)
