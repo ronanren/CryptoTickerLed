0dc179dd760cf2e9a82c4cbf563f5005ec5dbb07
ronanren <renouxpro@gmail.com>
2023-09-15 15:09:51 +0200

    update webapp

--- project/app/src/App.js
+++ project/app/src/App.js
@@ -3,14 +3,19 @@ import * as React from 'react'
 import { ChakraProvider } from '@chakra-ui/react'
 import { useState, useEffect } from 'react';
 import { Flex, Heading } from '@chakra-ui/react'
-import DisplayForm from './displayForm';
+import DisplayForm from './components/displayForm';
+import DisplayItem from './components/displayItem';
 
 function App() {
   const [displays, setDisplays] = useState([]);
 
-  const handleAddDisplay = async (newDisplay) => {
-    setDisplays(displays.displays.push(newDisplay));
+  const fetchDisplaysJSON = () => {
+    fetch('config_displays.json')
+      .then((res) => res.json())
+      .then((data) => { setDisplays(data); });
+  };
 
+  const writeDisplaysJSON = (displays) => {
     fetch('http://localhost:3001/write-json', {
       method: 'POST',
       headers: {
@@ -20,15 +25,37 @@ function App() {
     });
   };
 
-  const fetchDisplaysJSON = () => {
-    fetch('config_displays.json')
-      .then((res) => res.json())
-      .then((data) => { setDisplays(data); });
-  };
-
   useEffect(() => {
     fetchDisplaysJSON();
   }, []);
+
+  const handleAddDisplay = async (newDisplay) => {
+    let newDisplays = { displays: [...displays.displays, newDisplay] };
+    setDisplays(newDisplays);
+    writeDisplaysJSON(newDisplays);
+  };
+
+  const handleMoveUp = (index) => {
+    if (index > 0) {
+      let newDisplays = { displays: displays.displays.map((display, i) => i === index - 1 ? displays.displays[index] : i === index ? displays.displays[index - 1] : display) };
+      setDisplays(newDisplays);
+      writeDisplaysJSON(newDisplays);
+    }
+  };
+
+  const handleMoveDown = (index) => {
+    if (index < displays.displays.length - 1) {
+      let newDisplays = { displays: displays.displays.map((display, i) => i === index ? displays.displays[index + 1] : i === index + 1 ? displays.displays[index] : display) };
+      setDisplays(newDisplays);
+      writeDisplaysJSON(newDisplays);
+    }
+  };
+
+  const handleDelete = (index) => {
+    let newDisplays = { displays: displays.displays.filter((display, i) => i !== index) };
+    setDisplays(newDisplays);
+    writeDisplaysJSON(newDisplays);
+  };
 
   return (
     <ChakraProvider>
@@ -36,6 +63,7 @@ function App() {
         flexDirection="column"
         alignItems="center"
         height="100vh"
+        mb={5}
       >
         <Heading as="h1" my={4}>
           CryptoTickerLed
@@ -44,7 +72,19 @@ function App() {
         <Heading as="h4" fontSize={24} my={4}>
           Displays
         </Heading>
-        <pre>{JSON.stringify(displays, null, 2)}</pre>
+        {displays.displays && (
+          displays.displays.map((display, index) => (
+            <DisplayItem
+              key={index}
+              data={display}
+              index={index}
+              length={displays.displays.length}
+              onMoveUp={handleMoveUp}
+              onMoveDown={handleMoveDown}
+              onDelete={handleDelete}
+            />
+          ))
+        )}
       </Flex>
     </ChakraProvider>
   )
---
+++ project/app/src/components/displayItem.js
@@ -0,0 +1,83 @@
+import React from 'react';
+import { Button, Flex, Text } from '@chakra-ui/react';
+import { FaArrowUp, FaArrowDown } from 'react-icons/fa';
+
+const DisplayItem = ({ data, index, length, onMoveUp, onMoveDown, onDelete }) => {
+	const { type, id, symbol, interval } = data;
+
+	return (
+		<Flex
+			justifyContent="space-between"
+			alignItems="center"
+			p={3}
+			border="1px solid #ccc"
+			borderRadius="md"
+			mb={3}
+			width={380}
+		>
+			<Flex flexDirection="column" alignItems="flex-start" mx={8}>
+				<Flex flexDirection="row">
+					<Text fontWeight="bold">
+						Type:&nbsp;
+					</Text>
+					<Text>{type}</Text>
+				</Flex>
+
+				<Flex flexDirection="row">
+					<Text fontWeight="bold">
+						ID:&nbsp;
+					</Text>
+					<Text>{id}</Text>
+				</Flex>
+
+				<Flex flexDirection="row">
+					<Text fontWeight="bold">
+						Symbol:&nbsp;
+					</Text>
+					<Text>{symbol}</Text>
+				</Flex>
+
+				<Flex flexDirection="row">
+					<Text fontWeight="bold">
+						Interval:&nbsp;
+					</Text>
+					<Text>{interval}s</Text>
+				</Flex>
+			</Flex>
+
+			<Flex flexDirection="column" mx={4}>
+				<Button
+					onClick={() => onMoveUp(index)}
+					isDisabled={index === 0}
+					leftIcon={<FaArrowUp />}
+					variant="outline"
+					size="sm"
+					my={1}
+				>
+					Move Up
+				</Button>
+				<Button
+					onClick={() => onMoveDown(index)}
+					isDisabled={index === length - 1}
+					leftIcon={<FaArrowDown />}
+					variant="outline"
+					size="sm"
+					my={1}
+				>
+					Move Down
+				</Button>
+				<Button
+					onClick={() => onDelete(index)}
+					colorScheme="red"
+					variant="outline"
+					size="sm"
+					my={1}
+				>
+					Delete
+				</Button>
+			</Flex>
+		</Flex>
+	);
+};
+
+export default DisplayItem;
