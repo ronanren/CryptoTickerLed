d2dfe59b7ab43e98d22fcafb8036ff176706df09
ronanren <renouxpro@gmail.com>
2023-09-20 21:25:03 +0100

    run.sh works

--- project/run.sh
+++ project/run.sh
@@ -7,7 +7,6 @@ exec > /root/script.log 2> /root/script_error.log
 cd /root/CryptoTickerLed/project/ && ./run.py --led-gpio-mapping=adafruit-hat --led-rows=32 --led-cols=64 --led-slowdown-gpio=4 &
 
 # Move to the app directory and run server.js and start scripts
-cd /root/CryptoTickerLed/project/app
-/root/.bun/bin/bun run server.js & >> /root/server_json.log 2>> /root/server_json_error.log &
-node /root/.nvm/versions/node/v20.6.1/bin/serve -s build & >> /root/app.log 2>> /root/app_error.log &
+cd /root/CryptoTickerLed/project/app && /root/.bun/bin/bun run server.js & >> /root/server_json.log 2>> /root/server_json_error.log &
+cd /root/CryptoTickerLed/project/app && /root/.nvm/versions/node/v20.6.1/bin/node /root/.nvm/versions/node/v20.6.1/bin/serve -s build & >> /root/app.log 2>> /root/app_error.log &
 
